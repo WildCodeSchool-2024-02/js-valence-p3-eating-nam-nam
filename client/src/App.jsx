956981ccfcda12359abf87fed0559d892717bb52
derimer <jderimer@gmail.com>
2024-06-21 15:09:03 +0200

    page d'acceuil desktop et responsive

---
+++ client/src/App.jsx
@@ -0,0 +1,29 @@
+// import { useState } from "react";
+import Accueil from "./components/Accueil";
+
+export default function App() {
+  return (
+    <div>
+      <Accueil />
+    </div>
+  );
+}
+/* import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import Accueil from "./components/Accueil";
+
+i;
+
+export default function App() {
+  return (
+    <>
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Accueil />} />
+        </Routes>
+        <Footer />
+      </Router>
+    </>
+  );
+} */
