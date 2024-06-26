f4c315fa40140702c8d68d740ae9296c6d27934c
Hasbem <marvinmsb69@gmail.com>
2024-06-26 16:59:03 +0200

    ajout des modifications de jean-rene

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,5 @@
 import "./App.css";
-import Profile from "./pages/Profile";
 
-function App() {
-  return <Profile />;
-}
+function App() {}
 
 export default App;
--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import "../styles/Profile.css";
+import anna from "../assets/images/ANNA GUNN.jpg";
 
 export default function Profile() {
   const [formData, setFormData] = useState({
@@ -32,80 +33,73 @@ export default function Profile() {
 
   return (
     <>
-      <div className="navbar">NAVBAR</div>
       <div className="profile-container">
-        <img
-          src="/client/src/assets/images/ANNA GUNN.jpg"
-          alt="ANNA"
-          className="profile-image"
-        />
+        <img src={anna} alt="ANNA" className="profile-image" />
         <h2>MON PROFIL</h2>
         <div className="profile-info">
           <h2>pseudo</h2>
+          <p>Inscrite depuis le 22/01/2023</p>
         </div>
       </div>
       <h2 className="title">MES INFOS PERSO</h2>
       <div className="form-container">
-        <div className="form3-container">
-          <label htmlFor="pseudonyme">PSEUDONYME :</label>
-          <input
-            type="text"
-            id="pseudonyme"
-            name="pseudonyme"
-            value={formData.pseudonyme}
-            onChange={handleChange}
-            placeholder="Entrez votre pseudonyme"
-          />
+        <label htmlFor="pseudonyme">PSEUDONYME:</label>
 
-          <label htmlFor="prenom">PRÉNOM :</label>
-          <input
-            type="text"
-            id="prenom"
-            name="prenom"
-            value={formData.prenom}
-            onChange={handleChange}
-            placeholder="Entrez votre prénom"
-          />
+        <input
+          type="text"
+          id="pseudonyme"
+          name="pseudonyme"
+          value={formData.pseudonyme}
+          onChange={handleChange}
+          placeholder="Entrez votre pseudonyme"
+        />
 
-          <label htmlFor="nom">NOM :</label>
-          <input
-            type="text"
-            id="nom"
-            name="nom"
-            value={formData.nom}
-            onChange={handleChange}
-            placeholder="Entrez votre nom"
-          />
-        </div>
-        <div className="form2-container">
-          <label htmlFor="email">EMAIL :</label>
-          <input
-            type="email"
-            id="email"
-            name="email"
-            value={formData.email}
-            onChange={handleChange}
-            placeholder="Entrez votre email"
-          />
+        <label htmlFor="prenom">PRÉNOM:</label>
+        <input
+          type="text"
+          id="prenom"
+          name="prenom"
+          value={formData.prenom}
+          onChange={handleChange}
+          placeholder="Entrez votre prénom"
+        />
 
-          <label className="flex_aligne" htmlFor="password">
-            MOT DE PASSE :
-          </label>
-          <input
-            type="password"
-            id="password"
-            name="password"
-            value={formData.password}
-            onChange={handleChange}
-            placeholder="Entrez votre mot de passe"
-          />
-        </div>
+        <label htmlFor="nom">NOM:</label>
+        <input
+          type="text"
+          id="nom"
+          name="nom"
+          value={formData.nom}
+          onChange={handleChange}
+          placeholder="Entrez votre nom"
+        />
+
+        <label htmlFor="email">EMAIL:</label>
+        <input
+          type="email"
+          id="email"
+          name="email"
+          value={formData.email}
+          onChange={handleChange}
+          placeholder="Entrez votre email"
+        />
+
+        <label className="flex_aligne" htmlFor="password">
+          MOT DE PASSE:
+        </label>
+        <input
+          type="password"
+          id="password"
+          name="password"
+          value={formData.password}
+          onChange={handleChange}
+          placeholder="Entrez votre mot de passe"
+        />
       </div>
 
       <button type="submit" onClick={handleSubmit}>
         Enregistrer
       </button>
-      <div className="footer">FOOTER</div>
     </>
   );
 }
