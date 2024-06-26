329c6e74123af416361211c506b819aafdd60d06
Angéline SIMON <angelinesimon@protonmail.com>
2024-06-26 10:51:53 +0200

    creation de variables d'environnement et texte de bienvenue

---
+++ client/src/App.jsx
@@ -0,0 +1,14 @@
+import "./App.css";
+import ConsulterRecettesGenerales from "./components/ConsulterRecettesGenerales";
+
+function App() {
+  return (
+    <div>
+      <h1>Cabillaud</h1>
+      <p>101 résultats</p>
+      <ConsulterRecettesGenerales />
+    </div>
+  );
+}
+
+export default App;
