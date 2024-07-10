a6960d2bef13b2d23cfc5445ac2f3f4fa0325c83
Hasbem <109442038+Hasbem@users.noreply.github.com>
2024-07-10 11:01:02 +0200

    responsive navbar

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -10,43 +10,60 @@ function Navbar() {
   };
 
   return (
-    <nav>
-      <header>
-        <img className="navbar_logo" src={logo} alt="logo" width="90px" />
-      </header>
-      <div className="nav_content">
-        <input
-          type="text"
-          name="search_bar"
-          id="search_bar"
-          placeholder="Rechercher une recette, un ingrédient ..."
-        />
-        <div className="button_menu">
-          <button type="button">
-            <img src={accueil} width="30px" alt="Accueil" /> Accueil
-          </button>
-          <button type="button">
-            <img src={dernière} width="30px" alt="Dernières recettes" />{" "}
-            Dernières recettes ajoutées
-          </button>
-          <button type="button">
-            <img src={favoris} width="30px" alt="Mes recettes favorites" /> Mes
-            recettes favorites
-          </button>
-          <button type="button">
-            <img src={add} width="30px" alt="Ajouter une recette" /> Ajouter une
-            recette
-          </button>
+    <>
+      <nav>
+        <header>
+          <img className="navbar_logo" src={logo} alt="logo" />
+        </header>
+        <div className="nav_content">
+          <input
+            type="text"
+            name="search_bar"
+            id="search_bar"
+            placeholder="Rechercher une recette, un ingrédient ..."
+          />
+          <div className="button_menu">
+            <button type="button">
+              <img src={accueil} width="30px" alt="Accueil" /> Accueil
+            </button>
+            <button type="button">
+              <img src={dernière} width="30px" alt="Dernières recettes" />{" "}
+              Dernières recettes ajoutées
+            </button>
+            <button type="button">
+              <img src={favoris} width="30px" alt="Mes recettes favorites" />{" "}
+              Mes recettes favorites
+            </button>
+            <button type="button">
+              <img src={add} width="30px" alt="Ajouter une recette" /> Ajouter
+              une recette
+            </button>
+          </div>
         </div>
+        <button
+          className="login_button"
+          type="button"
+          onClick={redirectConnexion}
+        >
+          <img src={login} width="30px" alt="Connexion" />{" "}
+          <span>Connexion</span>
+        </button>
+      </nav>
+      <div className="mobile_navbar">
+        <button type="button">
+          <img src={accueil} width="30px" alt="Accueil" />
+        </button>
+        <button type="button">
+          <img src={dernière} width="30px" alt="Dernières recettes" />
+        </button>
+        <button type="button">
+          <img src={favoris} width="30px" alt="Mes recettes favorites" />
+        </button>
+        <button type="button">
+          <img src={add} width="30px" alt="Ajouter une recette" />
+        </button>
       </div>
-      <button
-        className="login_button"
-        type="button"
-        onClick={redirectConnexion}
-      >
-        <img src={login} width="30px" alt="Connexion" /> Connexion
-      </button>
-    </nav>
+    </>
   );
 }
 
