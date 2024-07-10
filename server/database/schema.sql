e7f360e2bebc92f2df3cc54d89663015002b0930
Hasbem <109442038+Hasbem@users.noreply.github.com>
2024-07-10 09:56:10 +0200

    mise à jour de la db

--- server/database/schema.sql
+++ server/database/schema.sql
@@ -1,13 +1,30 @@
 create table user (
-  id int unsigned primary key auto_increment not null,
-  username varchar(255) not null
-  email varchar(255) not null unique,
-  password varchar(255) not null
+  id INT UNSIGNED PRIMARY KEY AUTO_INCREMENT NOT NULL,
+  username VARCHAR(255) NOT NULL,
+  email VARCHAR(255) NOT NULL UNIQUE,
+  is_admin BOOLEAN NOT NULL DEFAULT 0,
+  password VARCHAR(255) NOT NULL
 );
 
 create table recettes (
-  id int unsigned primary key auto_increment not null,
-  title varchar(255) not null,
-  user_id int unsigned not null,
+  id INT unsigned PRIMARY KEY AUTO_INCREMENT NOT NULL,
+  title VARCHAR(255) NOT NULL,
+  user_id INT UNSIGNED NOT NULL,
+  image VARCHAR(255) NOT NULL,
+  ingredient VARCHAR(255) NOT NULL,
+  serving INT NOT NULL,
+  nutritional_values VARCHAR(255) NOT NULL,
+  is_validate BOOLEAN NOT NULL DEFAULT 0,
   foreign key(user_id) references user(id)
+);
+
+CREATE TABLE ingrédients (
+  id INT UNSIGNED PRIMARY KEY AUTO_INCREMENT NOT NULL,
+);
+
+CREATE TABLE steps (
+  id INT UNSIGNED PRIMARY KEY NOT NULL AUTO_INCREMENT,
+  recipe_id INT NOT NULL,
+  text TEXT NOT NULL,
+  FOREIGN KEY(recette_id) REFERENCES recettes(id)
 );
