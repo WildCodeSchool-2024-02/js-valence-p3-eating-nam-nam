8d65bd7be8f093344b982d41e964e8f7758dc89d
derimer <jderimer@gmail.com>
2024-07-12 10:34:11 +0200

    review

--- server/app/routers/api/router.js
+++ server/app/routers/api/router.js
@@ -8,7 +8,7 @@ const router = express.Router();
 
 const recetteController = require("../../controllers/recetteActions");
 
-router.get("/recette/:id", recetteController.read);
+router.get("/recettes/:id", recetteController.read);
 /* ************************************************************************* */
 
 module.exports = router;
