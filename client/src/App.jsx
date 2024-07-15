77f7ca88d62b50612d54ae73278681e96b237ac7
derimer <jderimer@gmail.com>
2024-07-15 16:45:53 +0200

    correction navbar

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,7 +2,7 @@ import { useState } from "react";
 import { Outlet } from "react-router-dom";
 import "./App.css";
 import Navbar from "./components/Navbar";
-import Footer from './components/Footer';
+
 
 function App() {
   const [searchQuery, setSearchQuery] = useState("");
@@ -11,7 +11,7 @@ function App() {
     <>
       <Navbar setSearchQuery={setSearchQuery} />
       <Outlet context={searchQuery} />
-      <Footer/>
+     
 
     </>
   );
