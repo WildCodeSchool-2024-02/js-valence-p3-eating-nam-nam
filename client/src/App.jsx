78355c38ccfaf9a8097a811e66adee8c1e3ee3bf
derimer <jderimer@gmail.com>
2024-07-15 18:34:00 +0200

    footer desktop et responsive

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -11,7 +11,7 @@ function App() {
     <>
       <Navbar setSearchQuery={setSearchQuery} />
       <Outlet context={searchQuery} />
-      <Footer setSearchQuery={setSearchQuery} />
+      <Footer />
     </>
   );
 }
--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -5,12 +5,12 @@ import "../styles/Footer.css";
 
 export default function Footer() {
   return (
-    <div className="button_menu">
+    <div className="button_footer">
       <NavLink
         to="/"
         className={({ isActive }) => (isActive ? "active" : null)}
       >
-        <img src={accueil} width="30px" alt="Accueil" /> Accueil
+        <img src={accueil} width="30px" alt="Accueil" />
       </NavLink>
 
       <NavLink
@@ -18,21 +18,18 @@ export default function Footer() {
         className={({ isActive }) => (isActive ? "active" : null)}
       >
         <img src={dernière} width="30px" alt="Dernières recettes" />
-        Dernières recettes ajoutées
       </NavLink>
       <NavLink
         to="/RecettesFavorites"
         className={({ isActive }) => (isActive ? "active" : null)}
       >
-        <img src={favoris} width="30px" alt="Mes recettes favorites" /> Mes
-        recettes favorites
+        <img src={favoris} width="30px" alt="Mes recettes favorites" />
       </NavLink>
       <NavLink
         to="/RecettesAjoutees"
         className={({ isActive }) => (isActive ? "active" : null)}
       >
-        <img src={add} width="30px" alt="Ajouter une recette" /> Ajouter une
-        recette
+        <img src={add} width="30px" alt="Ajouter une recette" />
       </NavLink>
     </div>
   );
