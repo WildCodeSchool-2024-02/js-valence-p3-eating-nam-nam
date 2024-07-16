f69d7d6ecb5314672015e089d2e50c9cb95fa529
Hasbem <109442038+Hasbem@users.noreply.github.com>
2024-07-16 18:01:37 +0200

    adaptation nom de classe et responsive

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -6,22 +6,22 @@ export default function Profile() {
   const user = useLoaderData();
 
   return (
-    <div className="profile-container">
-      <img src={anna} alt="ANNA" className="profile-image" />
-      <h2 className="pseudo">{user.username}</h2>
+    <div className="profil_container">
+      <img src={anna} alt="ANNA" className="profil_image" />
+      <h2 className="pseudo">{user.username || "Pseudo"}</h2>
       <h2 className="title">MES INFOS PERSO</h2>
-      <div className="form-container">
-        <h3 className="label-prenom">Prénom:</h3>
-        <span className="text-value">{user.name}</span>
+      <div className="info_container">
+        <h3 className="info_title">Prénom:</h3>
+        <span className="value">{user.name || ""}</span>
 
-        <h3 className="label-profil">Nom:</h3>
-        <span className="text-value">{user.last_name}</span>
+        <h3 className="info_title">Nom:</h3>
+        <span className="value">{user.last_name || ""}</span>
 
-        <h3 className="label-email">Email:</h3>
-        <span className="text-value">{user.email}</span>
+        <h3 className="info_title">Email:</h3>
+        <span className="value">{user.email || ""}</span>
 
-        <h3 className="label-profil">Date de naissance:</h3>
-        <span className="text-value">{user.birthdate}</span>
+        <h3 className="info_title">Date de naissance:</h3>
+        <span className="value">{user.birthdate}</span>
       </div>
     </div>
   );
