bff4efd6b999b36cd416f1615e874e74cb874e41
Hasbem <109442038+Hasbem@users.noreply.github.com>
2024-07-16 16:32:56 +0200

    ajustement css, supression mdp et ajout date de naissance

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -8,24 +8,20 @@ export default function Profile() {
   return (
     <div className="profile-container">
       <img src={anna} alt="ANNA" className="profile-image" />
-      <h2>MON PROFIL</h2>
-      <div className="profile-info">
-        <h2>{user.username || "Pseudo"}</h2>
-        <p>Inscrite depuis le 22/01/2023</p>
-      </div>
+      <h2 className="pseudo">{user.username || "Pseudo"}</h2>
       <h2 className="title">MES INFOS PERSO</h2>
       <div className="form-container">
-        <h3 className="label-prenom">PRÉNOM:</h3>
+        <h3 className="label-prenom">Prénom:</h3>
         <span className="text-value">{user.name || ""}</span>
 
-        <h3 className="label-profil">NOM:</h3>
+        <h3 className="label-profil">Nom:</h3>
         <span className="text-value">{user.last_name || ""}</span>
 
-        <h3 className="label-email">EMAIL:</h3>
+        <h3 className="label-email">Email:</h3>
         <span className="text-value">{user.email || ""}</span>
 
-        <h3 className="label-profil">MOT DE PASSE:</h3>
-        <span className="text-value">******</span>
+        <h3 className="label-profil">Date de naissance:</h3>
+        <span className="text-value">{user.birthdate}</span>
       </div>
     </div>
   );
