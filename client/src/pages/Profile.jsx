8f7a135eddab1edf8ea385580b8e29bc1d97ddcd
Hasbem <109442038+Hasbem@users.noreply.github.com>
2024-07-16 17:04:11 +0200

    ajustements

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -8,17 +8,17 @@ export default function Profile() {
   return (
     <div className="profile-container">
       <img src={anna} alt="ANNA" className="profile-image" />
-      <h2 className="pseudo">{user.username || "Pseudo"}</h2>
+      <h2 className="pseudo">{user.username}</h2>
       <h2 className="title">MES INFOS PERSO</h2>
       <div className="form-container">
         <h3 className="label-prenom">Prénom:</h3>
-        <span className="text-value">{user.name || ""}</span>
+        <span className="text-value">{user.name}</span>
 
         <h3 className="label-profil">Nom:</h3>
-        <span className="text-value">{user.last_name || ""}</span>
+        <span className="text-value">{user.last_name}</span>
 
         <h3 className="label-email">Email:</h3>
-        <span className="text-value">{user.email || ""}</span>
+        <span className="text-value">{user.email}</span>
 
         <h3 className="label-profil">Date de naissance:</h3>
         <span className="text-value">{user.birthdate}</span>
