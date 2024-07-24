9bed19e43a4b534a718e88e500030c3b178870ad
derimer <jderimer@gmail.com>
2024-07-24 15:00:40 +0200

    ajout de route

--- server/app/routers/api/router.js
+++ server/app/routers/api/router.js
@@ -5,6 +5,9 @@ const router = express.Router();
 /* ************************************************************************* */
 // Import And Use Routers Here
 /* ************************************************************************* */
+const stepsActions = require("../../controllers/stepsActions");
+
+router.get("/steps/:id", stepsActions.read);
 
 const recetteController = require("../../controllers/recetteActions");
 
