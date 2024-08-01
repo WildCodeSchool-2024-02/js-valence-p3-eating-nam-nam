ba44d90dab15c132869daa8e63f68f9c31ad20e7
Angéline SIMON <angelinesimon@protonmail.com>
2024-08-01 14:52:11 +0200

    revoir css Gestion utilisateur et Gestion recette

--- client/src/components/Admin.jsx
+++ client/src/components/Admin.jsx
@@ -3,14 +3,13 @@ import { Outlet, Link } from "react-router-dom";
 
 export default function Admin() {
   return (
-    <>
+    <div className="admin">
       <nav className="MenuAdmin">
         <Link to="/admin/">Gestion Recettes</Link>
-        <Link to="/admin/GestionIngredients">Gestion Ingredients</Link>
         <Link to="/admin/GestionUtilisateurs">Gestion Utilisateurs</Link>
       </nav>
 
       <Outlet />
-    </>
+    </div>
   );
 }
